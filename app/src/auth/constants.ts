12213591fc7f3849f1782421dbaffc4617d43d77
eduardohufg <eduardochavezmartin10@gmail.com>
2025-04-06 22:17:42 -0600

    update: requirements and url

--- app/src/auth/constants.ts
+++ app/src/auth/constants.ts
@@ -1,2 +1,2 @@
-export const WS_URL  = "ws://192.168.0.9:8000/ws/connection/img"
-export const WS_MOVE_URL = "ws://192.168.0.9:8000/ws/connection/move"
+export const WS_URL  = "ws://localhost:8000/ws/connection/img"
+export const WS_MOVE_URL = "ws://localhost:8000/ws/connection/move"
